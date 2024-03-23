c1f92f3feb0d0dcc43a93a39ac0f64732a61f6b4
Vaibhav Jamadagni <145725858+vjamadagni3@users.noreply.github.com>
2024-03-23 13:49:12 -0600

    Created frontend page

---
+++ ad-manager-frontend/src/assets/index.js
@@ -0,0 +1,7 @@
+const assets = {
+    images: {
+        logo: require('./images/adpulse_icon.png'),
+    }
+}
+
+export default assets;
---
+++ ad-manager-frontend/src/pages/changelog/ChangelogPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChangelogPage() {
+  return (
+    <div>ChangelogPage</div>
+  )
+}
+
+export default ChangelogPage;
