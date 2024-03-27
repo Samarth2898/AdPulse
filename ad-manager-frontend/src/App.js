af2e4e8769ac53d67937e86312caa3bf03facbc3
Kush2104 <kushal.1nagaraj.1@gmail.com>
2024-03-27 12:31:32 -0600

    Completed the table and added CORS in the backend

--- ad-manager-frontend/src/App.js
+++ ad-manager-frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/pages/HomePage';
-import PublisherPage from './components/pages/PublisherList'; // Import PublisherPage component
+import PublisherPage from './components/pages/PublisherPage'; // Import PublisherPage component
 
 function App() {
   return (
     <Router>
       <div style={{ display: 'flex' }}>
         <NavBar />
-        <div style={{ marginLeft: '240px', padding: '20px' }}> {/* Adjust marginLeft to match NavBar width */}
+        <div style={{ padding: '20px' }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/inventory/publisher" element={<PublisherPage />} /> {/* Add route for PublisherPage */}
