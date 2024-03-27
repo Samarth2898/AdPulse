73614de4864e29aae8133e7d47413d7588cfbee6
Vaibhav Jamadagni <145725858+vjamadagni3@users.noreply.github.com>
2024-03-26 19:01:42 -0600

    Code for publisher navbar

--- ad-manager-frontend/src/components/layout/PageWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PageWrapper() {
-  return (
-    <div>PageWrapper</div>
-  )
-}
-
-export default PageWrapper;
---
+++ ad-manager-frontend/src/pages/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Overview = () => {
+  return (
+    <div className='home'>
+      <h1>Overview</h1>
+      shdbgfjahdsfbgvjhdfbvhj
+    </div>
+  );
+};
+
+export default Overview;
