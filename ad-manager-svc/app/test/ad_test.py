7d11f7dd2c4c8161a5a89c9fd12c1227e1a3ec96
Aditi Kamatgi <aditihk@gmail.com>
2024-04-17 16:39:40 -0600

    rectified unit tests for ad and advertiser service

--- ad-manager-svc/app/test/ad_test.py
+++ ad-manager-svc/app/test/ad_test.py
@@ -1,6 +1,6 @@
-#unit test for ad-service
+# unit test for ad-service
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from app.services.ad_service import get_all_ads, create_ad, get_ad_by_id, get_ad_by_state, update_ad_state, update_ad
 
@@ -12,59 +12,124 @@ class TestAdService(unittest.TestCase):
         self.session_mock = MagicMock()
         self.mock_create_session.return_value = self.session_mock
 
-    def get_ad_id_by_name(self, json_data, ad_name):
-        for ad in json_data:
-            if ad.get('adname') == ad_name:
-                return ad.get('adid')
-
     def test_create_ad(self):
-        # Test case for create_ad function
         json_data = {
-        "adid": "A20240410162238070",
-        "adname": "test1Aditi",
-        "adpriority": 4,
-        "adstate": "CREATED",
-        "adtype": "GUARANTEED",
-        "advertiserid": "A12345",
-        "bidinfo": None,
-        "budget": None,
-        "campaignid": "C54321",
-        "createdat": "Wed, 10 Apr 2024 16:22:38 GMT",
-        "createdby": "kushal.1nagaraj.1@gmail.com",
-        "creativeid": "CR12345",
-        "enddate": "2024-03-17T21:05:56",
-        "frequencycaps": None,
-        "landingurl": "https://www.hotstar.com",
-        "startdate": "2024-03-17T21:05:56",
-        "targetinginfo": None,
-        "updatedat": "",
-        "updatedby": "aditihk@gmail.com"
+            "adid": "",
+            "adname": "test2024",
+            "adpriority": 4,
+            "adstate": "CREATED",
+            "adtype": "GUARANTEED",
+            "advertiserid": "A12345",
+            "bidinfo": None,
+            "budget": None,
+            "campaignid": "C54321",
+            "createdat": "Wed, 10 Apr 2024 16:22:38 GMT",
+            "createdby": "kushal.1nagaraj.1@gmail.com",
+            "creativeid": "CR12345",
+            "enddate": "2024-03-17T21:05:56",
+            "frequencycaps": None,
+            "landingurl": "https://www.hotstar.com",
+            "startdate": "2024-03-17T21:05:56",
+            "targetinginfo": None,
+            "updatedat": "",
+            "updatedby": "aditihk@gmail.com"
         }
 
-        # testing the create_ad function
         created_ad = create_ad(json_data)
         self.assertIsNotNone(created_ad)
         self.assertEqual(created_ad['adname'], json_data['adname'])
 
-        #testing the get all ads function
+
+    def test_get_all_ads(self):
         all_ads = get_all_ads()
         self.assertIsNotNone(all_ads)
         self.assertIn("test1Aditi", [ad['adname'] for ad in all_ads])
 
-        #testing the get ad by id function
-        ad_id = self.get_ad_id_by_name(all_ads, "test1Aditi")
+    def test_get_ad_by_id(self):
+        json_data = {
+            "adid": "",
+            "adname": "test1Aditi",
+            "adpriority": 4,
+            "adstate": "CREATED",
+            "adtype": "GUARANTEED",
+            "advertiserid": "A12345",
+            "bidinfo": None,
+            "budget": None,
+            "campaignid": "C54321",
+            "createdat": "Wed, 10 Apr 2024 16:22:38 GMT",
+            "createdby": "kushal.1nagaraj.1@gmail.com",
+            "creativeid": "CR12345",
+            "enddate": "2024-03-17T21:05:56",
+            "frequencycaps": None,
+            "landingurl": "https://www.hotstar.com",
+            "startdate": "2024-03-17T21:05:56",
+            "targetinginfo": None,
+            "updatedat": "",
+            "updatedby": "aditihk@gmail.com"
+        }
+
+        created_ad = create_ad(json_data)
+        ad_id = created_ad['adid']
         ad = get_ad_by_id(ad_id)
         self.assertIsNotNone(ad)
         self.assertEqual(ad['adid'], ad_id)
 
-        #testing the update ad state function
+    def test_update_ad_state(self):
+        json_data = {
+            "adid": "",
+            "adname": "test1Aditi",
+            "adpriority": 4,
+            "adstate": "CREATED",
+            "adtype": "GUARANTEED",
+            "advertiserid": "A12345",
+            "bidinfo": None,
+            "budget": None,
+            "campaignid": "C54321",
+            "createdat": "Wed, 10 Apr 2024 16:22:38 GMT",
+            "createdby": "kushal.1nagaraj.1@gmail.com",
+            "creativeid": "CR12345",
+            "enddate": "2024-03-17T21:05:56",
+            "frequencycaps": None,
+            "landingurl": "https://www.hotstar.com",
+            "startdate": "2024-03-17T21:05:56",
+            "targetinginfo": None,
+            "updatedat": "",
+            "updatedby": "aditihk@gmail.com"
+        }
+        created_ad = create_ad(json_data)
+        ad_id = created_ad['adid']
         state = "ACTIVE"
         update_ad_state(ad_id, state)
         updated_ad = get_ad_by_id(ad_id)
         self.assertIsNotNone(updated_ad)
         self.assertEqual(updated_ad['adstate'], state)
 
-        #testing the update ad function
+    def test_update_ad(self):
+        json_data = {
+            "adid": "",
+            "adname": "test1Aditi",
+            "adpriority": 4,
+            "adstate": "CREATED",
+            "adtype": "GUARANTEED",
+            "advertiserid": "A12345",
+            "bidinfo": None,
+            "budget": None,
+            "campaignid": "C54321",
+            "createdat": "Wed, 10 Apr 2024 16:22:38 GMT",
+            "createdby": "kushal.1nagaraj.1@gmail.com",
+            "creativeid": "CR12345",
+            "enddate": "2024-03-17T21:05:56",
+            "frequencycaps": None,
+            "landingurl": "https://www.hotstar.com",
+            "startdate": "2024-03-17T21:05:56",
+            "targetinginfo": None,
+            "updatedat": "",
+            "updatedby": "aditihk@gmail.com"
+        }
+
+        created_ad = create_ad(json_data)
+        ad_id = created_ad['adid']
+
         updated_json_data = {
             "adid": ad_id,
             "adname": "test1Aditi",
@@ -86,16 +151,19 @@ class TestAdService(unittest.TestCase):
             "updatedat": "",
             "updatedby": "aditihk@gmail.com"
         }
+
         update_ad(updated_json_data)
         updated_ad = get_ad_by_id(ad_id)
         self.assertIsNotNone(updated_ad)
         self.assertEqual(updated_ad['adpriority'], updated_json_data['adpriority'])
 
-    #testing the get ad by state function
+
+    def test_get_ad_by_state(self):
         state = "ACTIVE"
-        ad_state = get_ad_by_state(state)
-        self.assertIsNotNone(ad_state)
-        self.assertEqual(ad_state['adstate'], state)
+        ads = get_ad_by_state(state)
+        self.assertIsNotNone(ads)
+        self.assertIn("test1Aditi", [ad['adname'] for ad in ads])
+
 
 if __name__ == '__main__':
     unittest.main()
--- ad-manager-svc/app/test/advertiser_test.py
+++ ad-manager-svc/app/test/advertiser_test.py
@@ -1,7 +1,9 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
-from app.services.advertiser_service import get_all_advertisers, create_advertiser, get_advertiser_by_id, get_advertiser_by_state, update_advertiser_state, update_advertiser
+from app.services.advertiser_service import get_all_advertisers, create_advertiser, get_advertiser_by_id, \
+    get_advertiser_by_state, update_advertiser_state, update_advertiser
+
 
 class TestAdvertiserService(unittest.TestCase):
     def setUp(self):
@@ -15,61 +17,115 @@ class TestAdvertiserService(unittest.TestCase):
             if adv.get('advertisername') == ad_name:
                 return adv.get('advertiserid')
 
-    def test_advertiser(self):
+    def test_create_advertiser(self):
         # Test case for create_advertiser function
         json_data = {
-        "advertiserid" : "",
-        "advertisername" : "test1Aditi",
-        "industry" : "IT",
-        "brands" : ["test"],
-        "contactinfo" : {
-            "email" : "asdf@gmail.com"
-        },
-        "advertisertype" : "GUARANTEED",
-        "createdby" : "aditi@gmail.com ",
-        "updatedby" : "admin",
-        "createdat" : "2024-03-17T21:05:56",
-        "updatedat" : "2024-03-17T21:05:56",
-        "advertiserstate": "CREATED"
+            "advertiserid": "",
+            "advertisername": "test1Aditi",
+            "industry": "IT",
+            "brands": ["test"],
+            "contactinfo": {
+                "email": "asdf@gmail.com"
+            },
+            "advertisertype": "GUARANTEED",
+            "createdby": "aditi@gmail.com ",
+            "updatedby": "admin",
+            "createdat": "2024-03-17T21:05:56",
+            "updatedat": "2024-03-17T21:05:56",
+            "advertiserstate": "CREATED"
         }
 
-        # Testing the create_advertiser function
         created_advertiser = create_advertiser(json_data)
         self.assertIsNotNone(created_advertiser)
         self.assertEqual(created_advertiser['advertisername'], json_data['advertisername'])
 
-        # Testing the get all advertisers function
+    def test_get_all_advertisers(self):
         all_advertisers = get_all_advertisers()
         self.assertIsNotNone(all_advertisers)
         self.assertIn("test1Aditi", [advertiser['advertisername'] for advertiser in all_advertisers])
 
-        # Testing the get advertiser by id function
-        adv_id = self.get_adv_id_by_name(all_advertisers, "test1Aditi")
+    def test_get_advertiser_by_id(self):
+        json_data = {
+            "advertiserid": "",
+            "advertisername": "test1Aditi",
+            "industry": "IT",
+            "brands": ["test"],
+            "contactinfo": {
+                "email": "asdf@gmail.com"
+            },
+            "advertisertype": "GUARANTEED",
+            "createdby": "aditi@gmail.com ",
+            "updatedby": "admin",
+            "createdat": "2024-03-17T21:05:56",
+            "updatedat": "2024-03-17T21:05:56",
+            "advertiserstate": "CREATED"
+        }
+        created_advertiser = create_advertiser(json_data)
+        adv_id = created_advertiser['advertiserid']
         advertiser = get_advertiser_by_id(adv_id)
         self.assertIsNotNone(advertiser)
         self.assertEqual(advertiser['advertiserid'], adv_id)
 
-        # Testing update advertiser state function
+    def test_update_advertiser_state(self):
+        json_data = {
+            "advertiserid": "",
+            "advertisername": "test1Aditi",
+            "industry": "IT",
+            "brands": ["test"],
+            "contactinfo": {
+                "email": "asdf@gmail.com"
+            },
+            "advertisertype": "GUARANTEED",
+            "createdby": "aditi@gmail.com ",
+            "updatedby": "admin",
+            "createdat": "2024-03-17T21:05:56",
+            "updatedat": "2024-03-17T21:05:56",
+            "advertiserstate": "CREATED"
+        }
+
+        created_advertiser = create_advertiser(json_data)
+        adv_id = created_advertiser['advertiserid']
         state = "INACTIVE"
         update_advertiser_state(adv_id, state)
         updated_advertiser = get_advertiser_by_id(adv_id)
         self.assertIsNotNone(updated_advertiser)
         self.assertEqual(updated_advertiser['advertiserstate'], state)
 
-        # Testing update advertiser function
-        updated_json_data = { "advertiserid" : adv_id,
-        "advertisername" : "test1Aditi",
-        "industry" : "IT",
-        "brands" : ["test45"],
-        "contactinfo" : {
-            "email" : "asdf@gmail.com"
-        },
-        "advertisertype" : "GUARANTEED",
-        "createdby" : "aditi@gmail.com ",
-        "updatedby" : "admin",
-        "createdat" : "2024-03-17T21:05:56",
-        "updatedat" : "2024-03-17T21:05:56",
-        "advertiserstate": "INACTIVE"}
+    def test_update_advertiser(self):
+        json_data = {
+            "advertiserid": "",
+            "advertisername": "test1Aditi",
+            "industry": "IT",
+            "brands": ["test"],
+            "contactinfo": {
+                "email": "asdf@gmail.com"
+            },
+            "advertisertype": "GUARANTEED",
+            "createdby": "aditi@gmail.com ",
+            "updatedby": "admin",
+            "createdat": "2024-03-17T21:05:56",
+            "updatedat": "2024-03-17T21:05:56",
+            "advertiserstate": "CREATED"
+        }
+
+        created_advertiser = create_advertiser(json_data)
+        adv_id = created_advertiser['advertiserid']
+
+        updated_json_data = {
+            "advertiserid": adv_id,
+            "advertisername": "test1Aditi",
+            "industry": "IT",
+            "brands": ["test45"],
+            "contactinfo": {
+                "email": "asdf1234@gmail.com"
+            },
+            "advertisertype": "GUARANTEED",
+            "createdby": "aditi@gmail.com ",
+            "updatedby": "admin",
+            "createdat": "2024-03-17T21:05:56",
+            "updatedat": "2024-03-17T21:05:56",
+            "advertiserstate": "CREATED"
+        }
 
         update_advertiser(updated_json_data)
         updated_advertiser = get_advertiser_by_id(adv_id)
@@ -77,9 +133,12 @@ class TestAdvertiserService(unittest.TestCase):
         self.assertEqual(updated_advertiser['brands'], updated_json_data['brands'])
         self.assertEqual(updated_advertiser['contactinfo'], updated_json_data['contactinfo'])
 
-        # testing the get advertiser by state function
+    def test_get_advertiser_by_state(self):
         adv_state = "INACTIVE"
         advs = get_advertiser_by_state(adv_state)
         self.assertIsNotNone(advs)
         self.assertIn("test1Aditi", [advertiser['advertisername'] for advertiser in advs])
 
+
+if __name__ == '__main__':
+    unittest.main()
